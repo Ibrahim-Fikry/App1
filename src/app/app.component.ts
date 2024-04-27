516c6a538047025e88eff5ac7f5645185e75e2a3
Ibrahim Fikry <ibrahim.fikry2050@gmail.com>
2024-04-28 01:54:50 +0300

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, Directive } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+ 
+  
+})
+export class AppComponent {
+  title = 'App1';
+}
